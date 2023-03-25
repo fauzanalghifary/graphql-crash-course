bed566905d5ec0c186f3c7c895da8a941f2f8126
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-03-25 19:57:22 +0700

    finish section 02

---
+++ Section 02 Fundamental/server/server.js
@@ -0,0 +1,23 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  schema {
+    query: Query
+  }
+
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => "Hello World!",
+  },
+};
+
+export { typeDefs, resolvers };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await server.listen({ port: 9000 });
+console.log(`Server is running on ${url}`);
