6daaa521771b57f5ace4b9b98780c74c7fb465a0
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-03-29 00:24:06 +0700

    finish section 05

--- Section 03-08 Job Board Project/server/resolvers.js
+++ Section 03-08 Job Board Project/server/resolvers.js
@@ -1,5 +1,11 @@
 import { Company, Job } from "./db.js";
 
+function rejectIf(condition, message) {
+  if (condition) {
+    throw new Error(message);
+  }
+}
+
 export const resolvers = {
   Query: {
     company: (_root, args) => Company.findById(args.id),
@@ -8,17 +14,25 @@ export const resolvers = {
   },
 
   Mutation: {
-    createJob: (_root, { input }) => {
-      const job = Job.create(input);
+    createJob: (_root, { input }, { user }) => {
+      if (!user) {
+        throw new Error("Unauthorized");
+      }
+      const job = Job.create({ ...input, companyId: user.companyId });
       return job;
     },
-    deleteJob: (_root, { id }) => {
-      const job = Job.delete(id);
-      return job;
+    deleteJob: async (_root, { id }, { user }) => {
+      rejectIf(!user, "Unauthorized");
+
+      const job = await Job.findById(id);
+      rejectIf(job.companyId !== user.companyId, "Job not found");
+      return Job.delete(id);
     },
-    updateJob: (_root, { input }) => {
-      const job = Job.update(input);
-      return job;
+    updateJob: async (_root, { input }, { user }) => {
+      rejectIf(!user, "Unauthorized");
+      const job = await Job.findById(input.id);
+      rejectIf(job.companyId !== user.companyId, "Job not found");
+      return Job.update({ ...input, companyId: user.companyId });
     },
   },
 
